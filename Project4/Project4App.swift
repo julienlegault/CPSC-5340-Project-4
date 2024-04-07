d58671e37ac18db84c80b98cec11913ca3b8846f
julienlegault <julien.p.legault@gmail.com>
2024-04-07 15:33:19 -0700

    Created project

---
+++ Project4/Project4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project4App.swift
+//  Project4
+//
+//  Created by Julien on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
